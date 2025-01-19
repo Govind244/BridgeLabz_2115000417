90fe8334646b2a43451fb3dd49a3282d3aaa830d
Govind244 <govindsaraswat244@gmail.com>
2025-01-19 22:52:58 +0530

    celsius fahrenheit file

---
+++ CelsiusToFahrenheit.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class CelsiusToFahrenheit {
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	double celsius = sc.nextDouble();
+    	System.out.println((celsius * 9 / 5) + 32);
+	}
+}
