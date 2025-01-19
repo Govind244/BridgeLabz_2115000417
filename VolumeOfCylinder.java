86ed13f478bdfe577f8ef4046c850ce29af69aa8
Govind244 <govindsaraswat244@gmail.com>
2025-01-19 23:10:15 +0530

    Cylinder Volume file

---
+++ VolumeOfCylinder.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class VolumeOfCylinder {
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	double radius = sc.nextDouble();
+    	double height = sc.nextDouble();
+    	System.out.println(Math.PI * radius * radius * height);
+	}
+}
