81911ac9865a2727bb48d36e7b7659232652cbd1
Govind244 <govindsaraswat244@gmail.com>
2025-01-19 23:49:49 +0530

    km to Miles file

---
+++ KilometersToMiles.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class KilometersToMiles {
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	double kilometers = sc.nextDouble();
+    	System.out.println(kilometers * 0.621371);
+	}
+}
